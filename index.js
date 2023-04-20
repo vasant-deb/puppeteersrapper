02d0dd2fdb0bf130e76b9d64318ca01e41928d80
vasant-deb <iamvasantsharma@gmail.com>
2023-04-19 20:51:26 -0600

    newone

--- index.js
+++ index.js
@@ -15,27 +15,7 @@ app.get('/about', (req, res) => {
   res.send('This is my about route..... ')
 })
 
-app.get('/newscrape', async (req, res) => {
-  try {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    //await page.setViewport({ width: 1920, height: 1080 });
-    await page.setViewport({ width: 0, height: 0 });
-    await page.goto('https://www.rogers.com/consumer/profile/signin');
-  
-    console.log(await page.title());
-  
-   // await browser.close();
-  
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    const message = 'It works!\n';
-    const version = 'NodeJS ' + process.versions.node + '\n';
-    const response = [message, version].join('\n');
-    res.end(response);
-  } catch (error) {
-    return res.status(500).json({ error: true, message: error.message });
-  }
-})
+
 
 // Export the Express API
 module.exports = app
